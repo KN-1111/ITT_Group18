151e724ada79113b6edb470f72e5070ab5eededc
Kisari <seikakukisari@gmail.com>
2022-05-07 09:56:31 +0700

    About us

--- main/main.js
+++ main/main.js
@@ -37,3 +37,49 @@
 
   window.addEventListener("scroll", checkScroll);
 })();
+
+// var person_list = document.querySelectorAll(".person-container");
+// person_list.forEach((person, index) => {
+//   person.addEventListener("mouseenter", (event) => {
+//     var remove_list = person_list.splice(1, 1);
+//     remove_list.forEach((item) => {
+//       item.style.opacity = "0.4";
+//     });
+//   });
+// });
+
+var person_list = document.querySelectorAll(".person-container");
+// for (person in person_list) {
+//   person_list[person].onmouseover = function () {
+//     for (item in person_list) {
+//       person_list[item].style.opacity = "0.4";
+//     }
+//   };
+//   person_list[person].onmouseout = function () {
+//     for (item in person_list) {
+//       if (person_list[person] != person_list[item]) {
+//         // console.log("first item", person_list[person]);
+//         // console.log("second item", person_list[item]);
+//         // console.log(item !== person);
+//         // person_list[item].style.opacity = "0.4";
+//       }
+//       person_list[item].style.opacity = "0.4";
+//     }
+//   };
+// }
+person_list.forEach((element) => {
+  element.addEventListener("mouseover", (event) => {
+    person_list.forEach((item) => {
+      if (item != element) {
+        item.style.opacity = "0.4";
+      }
+    });
+  });
+  element.addEventListener("mouseout", (event) => {
+    person_list.forEach((item) => {
+      if (item != element) {
+        item.style.opacity = "1";
+      }
+    });
+  });
+});
